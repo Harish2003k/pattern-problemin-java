359854f0b14d0700a2f0a085af89e0eef30cbba4
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-10-30 23:31:15 +0530

    Add files via upload
    drecresing and increasing pattern star

---
+++ patternThird.java
@@ -0,0 +1,14 @@
+class PatternThird{
+    public static void main(String args[]){
+        int n=5,i,j;
+        for(i=1;i<=n;i++){
+            for(j=i;j<=n;j++){   //for spacing 
+                System.out.print(" ");
+            }
+                for(j=1;j<=i;j++){ // for printing star 
+                    System.out.print("*");
+                }
+                System.out.println(); // printing new line
+        }
+    }
+}
