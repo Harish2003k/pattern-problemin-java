09030de9ac22cdeef1b413dea692e9b829811131
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-11-06 18:36:27 +0530

    Add files via upload
    Pattern in Square

---
+++ SquarePattern.java
@@ -0,0 +1,10 @@
+class PatternTwo{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(i); // in place of i put j to give another pattern
+            }
+            System.out.println();
+        }
+    }
+}
