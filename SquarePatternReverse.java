e1a767508516151dda97d736b047d67ab626429c
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-11-06 18:42:25 +0530

    Add files via upload
    Square in Reverse Pattern

---
+++ SquarePatternReverse.java
@@ -0,0 +1,10 @@
+class SquarePattern{
+    public static void main(String args[]){
+        for(int i=5;i>=1;i--){
+            for(int j=5;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
