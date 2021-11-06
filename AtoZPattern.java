46cfbc2977e88ac478cbc516290ba12e3b8b1dcd
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-11-06 19:01:26 +0530

    Add files via upload
    A to Z pattern in small letter as well as capital letter

---
+++ AtoZPattern.java
@@ -0,0 +1,9 @@
+class PatternAtoZ{
+    public static void main(String args[]){
+        char c;
+        for(c='A';c<='Z';c++){   //c='a';c<='z';c++ in small Letter.
+            System.out.println(c);
+        }
+        System.out.println();
+    }
+}
